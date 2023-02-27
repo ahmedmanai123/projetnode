40c4f84cbcc3be1b573ca16999ba810fc8b6ed1a
ahmed manai <elmanai5400@gmail.com>
2023-02-27 17:27:40 +0100

    mon premier commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express=require('express'); 
+const mongoose =require("mongoose") 
+const dotenv =require('dotenv') 
+dotenv.config() 
+const app = express(); 
+const categorieRoutera=require("./routes/categorie.route")
+
+//BodyParser Middleware
+app.use(express.json()); 
+const categorieRouter =require("./routes/categorie.route") 
+app.use('/api/categories', categorieRouter); 
+const scategorieRouter =require("./routes/scategorie.route") 
+app.use('/api/scategories', scategorieRouter);
+const articleRouter =require("./routes/article.route") 
+app.use('/api/articles', articleRouter);
+mongoose.set("strictQuery", false); 
+// Connexion à la base données
+mongoose.connect(process.env.DATABASECLOUD,{ 
+useNewUrlParser: true, 
+useUnifiedTopology: true
+ }) 
+ .then(() => {console.log("Connexion à la base de données réussie"); 
+
+}).catch(err => { 
+console.log('Impossible de se connecter à la base de données', err); 
+process.exit(); 
+}); 
+app.get("/",(req,res)=>{ 
+res.send("bonjour"); 
+}); 
+app.listen(process.env.PORT, () => { 
+console.log(`Server is listening on port ${process.env.PORT}`); }); 
+module.exports = app;
